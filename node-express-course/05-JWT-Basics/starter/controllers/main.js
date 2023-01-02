b7fae5e3b716d22caf3ed52d395d5500c89f229b
katoragashua <katoragashua@outlook.com>
2023-01-02 12:32:56 +0100

    Starting with JWTs

--- node-express-course/05-JWT-Basics/starter/controllers/main.js
+++ node-express-course/05-JWT-Basics/starter/controllers/main.js
@@ -1,12 +1,20 @@
 const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+const { config } = require("dotenv");
+config();
 
 const login = async (req, res, next) => {
   const { username, password } = req.body;
   if (!username.trim() || !password.trim()) {
     throw new CustomAPIError("Invalid username or password", 400);
   }
+  const id = new Date().getTime();
+  console.log(id);
+  const token = jwt.sign({ username, id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
   console.log(username, password);
-  res.send("Fake login/Register/Sign up Route");
+  res.status(200).json({ msg: "User Created", token });
 };
 
 const dashboard = async (req, res, next) => {
