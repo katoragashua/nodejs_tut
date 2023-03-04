9829e97000a0b90afd322fbe9627953bb428d63c
katoragashua <katoragashua@outlook.com>
2023-03-04 21:08:10 +0100

    Added extra security with cors and helmet

--- node-express-course/06-jobs-api/starter/app.js
+++ node-express-course/06-jobs-api/starter/app.js
@@ -14,8 +14,28 @@ const notFoundMiddleware = require("./errors/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const authMiddleware = require("./middleware/authentication");
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
+
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
 // extra packages
 
 // Route Middlewares
